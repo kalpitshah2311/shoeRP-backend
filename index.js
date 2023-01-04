ed425442b77c76b9c411ebd8be64c15540ad2df3
Kalpit Shah <72689203+kalpitshah2311@users.noreply.github.com>
2023-01-05 00:35:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!'+req.ip);
 })
 
 app.use('/api/wholesaler', require('./routes/wholesaler'))
